cf1a6f52812d1e8d937059d69bd24ed20edd9242
mohmdrezaei <mhwmdrz9@gmail.com>
2025-04-06 17:03:46 +0330

    Hero section design

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,25 @@
-
 export default function Home() {
   return (
-   <h1>task</h1>
+    <main className="h-svh w-[1200px] m-auto ">
+      <div className="flex  pt-7  ">
+        <div className="mt-16">
+          <h2 className="font-[600] text-2xl">کمپین های بازاریابی و تبلیغاتی</h2>
+          <p className="w-[486px] font-[500] mt-5 text-lg text-[#303030]">
+            آیا به دنبال راهی مطمئن برای دیده شدن برندتان هستید؟ مشاوره رایگان
+            ما به شما کمک می‌کند تا مسیر موفقیت را پیدا کنید. همین حالا قدم اول
+            را بردارید!
+          </p>
+          <p className="font-[500] mt-5 text-base text-[#515151] leading-10">جهت دریافت مشاوره رایگان با شماره‌ی زیر تماس بگیرید</p>
+          <button className="bg-[#7A3DE2] text-white py-2 px-3 rounded-lg mt-5">دریافت مشاوره</button>
+        </div>
+        <div>
+          <img src="/images/hero.svg" alt="Hero"></img>
+        </div>
+      </div>
+
+      <div className="bg-[#F9F9F9]">
+       
+      </div>
+    </main>
   );
 }
